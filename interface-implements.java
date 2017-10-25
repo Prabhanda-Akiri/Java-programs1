e699b95150dda6cb3c93dbb50149838426aeb254
Akiri Prabhanda <31247199+Prabhanda-Akiri@users.noreply.github.com>
2017-10-25 16:49:33 +0530

    Update interface-implements.java

--- interface-implements.java
+++ interface-implements.java
@@ -15,25 +15,41 @@ class w
 	interface I1
 	{
 		public void m1();
+		public void m2();
 	} 
 
 	interface I2 extends I1
 	{
+		public void m3();
 		public void m6();
 
 	}
 
 
-	class a implements I2,I1
+	abstract class a implements I2,I1
 	{
 		public void m1()
 		{
-			System.out.println("Inside overriden method m1");
+			System.out.println("\nInside overriden method m1");
 		}
 
 		public void m6()
 		{
-			System.out.println("Inside overriden method m6");
+			System.out.println("\nInside overriden method m6");
+		}
+	}
+
+	class b extends a
+	{
+
+		public void m2()
+		{
+			System.out.println("\noverriden method m2 in extended class b");
+		}
+
+		public void m3()
+		{
+			System.out.println("\noverriden method m3 in extended class b");
 		}
 	}
 }
@@ -43,10 +59,13 @@ public class B
 	public static void main(String[] args)
 	{
 		w o=new w();
-		w.a o1=o.new a();
+		w.b o1=o.new b();
 
 		o1.m1();
 		o1.m6();
+
+		o1.m2();
+		o1.m3();
 	}
 
 
