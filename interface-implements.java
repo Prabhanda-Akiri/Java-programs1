bfedd4f0e6a2f1ff934d94e8c9d56bb47fd09d46
Akiri Prabhanda <31247199+Prabhanda-Akiri@users.noreply.github.com>
2017-10-25 16:36:08 +0530

    Update interface-implements.java

--- interface-implements.java
+++ interface-implements.java
@@ -1,8 +1,10 @@
+
 import java.util.Scanner;
 
 class w
 {
 	int a,b,c;
+
 	w()
 	{
 		a=0;
@@ -10,33 +12,30 @@ class w
 		c=0;
 	}
 
-Interface I1
-{
-	abstract void m1();
-	abstract void m2();
-	abstract void m3();
-	abstract void m4();
-} 
-
-Interface I2 extends I1
-{
-	abstract void m6();
-	abstract void m7();
-}
-
-
-abstract class a implements I2
-{
-	void m1()
+	interface I1
 	{
-		System.out.println("Inside overriden method m1");
+		public void m1();
+	} 
+
+	interface I2 extends I1
+	{
+		public void m6();
+
 	}
 
-	void m6()
+
+	class a implements I2,I1
 	{
-		System.out.println("Inside overriden method m6");
+		public void m1()
+		{
+			System.out.println("Inside overriden method m1");
+		}
+
+		public void m6()
+		{
+			System.out.println("Inside overriden method m6");
+		}
 	}
-}
 }
 
 public class B
