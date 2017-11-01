f21a3887175445d76132cf4870c3b0c790f3f105
Akiri Prabhanda <31247199+Prabhanda-Akiri@users.noreply.github.com>
2017-11-01 14:10:31 +0530

    Create Pass.java

---
+++ Pass.java
@@ -0,0 +1,18 @@
+package Mypack3;
+
+public class Pass
+{
+	int a;
+
+	public Pass()
+	{
+		a=5;
+	}
+
+	public int show()
+	{
+		System.out.println("The value of 'a' present in the package Mypack3 is returned to the external class");
+
+		return a;
+	}
+}
